9e4f2c96fca40f9b8e441f44690a4c11a262ce47
hassangomaa <hassangomaa974@gmail.com>
2025-02-02 16:50:18 +0200

    Add helper functions and corresponding tests; update PHPUnit configuration

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!function_exists('calc')) {
+    /**
+     * Sum two numbers.
+     *
+     * @param int|float $n1
+     * @param int|float $n2
+     * @return int|float
+     */
+    function calc($n1, $n2)
+    {
+        return $n1 + $n2;
+    }
+}
