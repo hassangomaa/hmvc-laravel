1948dc1deaac9d7a524e5bb04d858095361dab3f
hassangomaa <hassangomaa974@gmail.com>
2025-02-17 14:08:32 +0200

    Add Vue.js integration with Counter component and update routing to use home view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import { createApp } from "vue";
+import Counter from "./components/Counter.vue";
+
+const app = createApp({
+  components: {
+	Counter
+  }
+});
+app.component("Counter", Counter);
+app.mount("#app");
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+
+    
+    <Counter />
+
+</x-layouts.app>
